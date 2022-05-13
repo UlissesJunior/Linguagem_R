61e729e0c107a3b75829f15bd7622a7f0428668d
UlissesJunior <ulissesjunior2005@gmail.com>
2022-05-13 13:17:01 -0300

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from "./img/logo.png";
+import "./styles/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Trabalho sobre Linguagem R</p>
+        <p>
+          Cauã Gabriel Pereira da Silva
+          <br />
+          Kamily Maria Barboza Leduino
+          <br />
+          Luiz Felipe Silvestre de Castro
+          <br />
+          Ulisses Douglas de Paula Asssis Junior
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
