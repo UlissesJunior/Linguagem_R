15723a1d1d979def46b28c5e5f5eda11f1385625
UlissesJunior <ulissesjunior2005@gmail.com>
2022-06-12 15:09:46 -0300

    i need to arranged the use state for each accordion

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,15 +1,21 @@
 import "./styles/app.css";
 import Nav from "./components/navbar.jsx";
 import Footer from "./components/footer.jsx";
+import { useState } from "react";
 
-const accordion = document.getElementsByClassName('acc-box')
-for (var i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', () => {
-    this.classList.toggle('active')
-  })
-}
+// const useEffect(() => {
+//   const accordion = document.getElementsByClassName('acc-box')
+// for (var i=0; i<accordion.length; i++) {
+//   accordion[i].addEventListener('click', () => {
+//     this.classList.toggle('active')
+//   })
+// }
+// }, [])
 
 function Curso() {
+  const [show, setShow] = useState(false);
+  const Each = e => setShow(e.target.value);
+  // onChange={Each}
   return (
     <>
       <Nav />
@@ -26,20 +32,53 @@ function Curso() {
         <div className="container-box">
           <div className="accordion">
             <div className="acc-box">
-              <div className="acc-label">Aula 1</div>
-              <div className="acc-content"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio non ducimus odio laboriosam, minus quia sequi quasi eum assumenda hic facere, a eos necessitatibus possimus dolorum quis illum voluptates? Eos!</p></div>
+              <div className="acc-label" onClick={() => setShow(!show)} >
+                Aula 1
+              </div>
+              {show ? (
+                <div className="acc-content active">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Optio non ducimus odio laboriosam, minus quia sequi quasi
+                    eum assumenda hic facere, a eos necessitatibus possimus
+                    dolorum quis illum voluptates? Eos!
+                  </p>
+                </div>
+              ) : null}
             </div>
           </div>
           <div className="accordion">
             <div className="acc-box">
-              <div className="acc-label">Aula 2</div>
-              <div className="acc-content"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio non ducimus odio laboriosam, minus quia sequi quasi eum assumenda hic facere, a eos necessitatibus possimus dolorum quis illum voluptates? Eos!</p></div>
+              <div className="acc-label" onClick={() => setShow(!show)}>
+                Aula 2
+              </div>
+              {show ? (
+                <div className="acc-content active">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Optio non ducimus odio laboriosam, minus quia sequi quasi
+                    eum assumenda hic facere, a eos necessitatibus possimus
+                    dolorum quis illum voluptates? Eos!
+                  </p>
+                </div>
+              ) : null}
             </div>
           </div>
           <div className="accordion">
             <div className="acc-box">
-              <div className="acc-label">Aula 3</div>
-              <div className="acc-content"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio non ducimus odio laboriosam, minus quia sequi quasi eum assumenda hic facere, a eos necessitatibus possimus dolorum quis illum voluptates? Eos!</p></div>
+              <div className="acc-label" onClick={() => setShow(!show)}>
+                Aula 3
+              </div>
+              {show ? (
+                <div className="acc-content active">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Optio non ducimus odio laboriosam, minus quia sequi quasi
+                    eum assumenda hic facere, a eos necessitatibus possimus
+                    dolorum quis illum voluptates? Eos!
+                  </p>
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
