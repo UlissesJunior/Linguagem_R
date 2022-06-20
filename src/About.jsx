f5d06fa4f4feb3a5e8dad1222eac1cbbbd7d1be0
UlissesJunior <ulissesjunior2005@gmail.com>
2022-06-20 08:56:35 -0300

    add content in about - without responsive

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,10 @@
 import "./styles/app.css";
 import Nav from "./components/navbar.jsx";
 import Footer from "./components/footer.jsx";
+import Kamily from "./img/about/Kamily.jpg";
+import Caua from "./img/about/Caua.jpg";
+import Ulisses from "./img/about/Ulisses.jpg";
+import Luiz from "./img/about/Luiz.jpg";
 
 function About() {
   return (
@@ -17,6 +21,34 @@ function About() {
       </div>
       <div className="container">
         <div className="container-box">
+          <div className="row">
+            <img src={Caua} alt="Cauã"/>
+            <div className="row-text">
+              <h1>Cauã Gabriel Pereira da Silva</h1>
+              <p>Edição dos vídeos</p>
+            </div>
+          </div>
+          <div className="row">
+            <img src={Kamily} alt="Kamily"/>
+            <div className="row-text">
+              <h1>Kamily Maria Barboza Leduino</h1>
+              <p>Pesquisas</p>
+            </div>
+          </div>
+          <div className="row">
+            <img src={Luiz} alt="Luiz"/>
+            <div className="row-text">
+              <h1>Luiz Felipe Silvestre de Castro</h1>
+              <p>Gravação dos vídeos</p>
+            </div>
+          </div>
+          <div className="row">
+            <img src={Ulisses} alt="Ulisses"/>
+            <div className="row-text">
+              <h1>Ulisses Douglas de Paula Assis Junior</h1>
+              <p>Desenvolvimento Web</p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
