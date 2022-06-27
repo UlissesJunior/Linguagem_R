4afc09da0220d5cc9bf669b5dc7c2ae7f3dea8d2
UlissesJunior <ulissesjunior2005@gmail.com>
2022-06-27 15:25:24 -0300

    add link video

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -10,7 +10,7 @@ function Curso() {
     {
       name: "Aula 1 - O que é R?",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/1CIWJ_2ugbshboqcr59BIX81eOLV-JqDJ/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/11uJHGyyoL3ZNZCB4ikW7YfX-Wctmw4Ps/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "/docs",
       contentBibliografia: "Bibliografia",
@@ -20,7 +20,7 @@ function Curso() {
     {
       name: "Aula 2 - Variáveis em R",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/1REyrefx7l49PmhDXaPcSFfrLkFU-_v7d/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/1ZFWX8Qp2e7C9s8i2YHLyrlu_YpQE5Gva/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "https://docs.google.com/document/d/1pWaPidqL5x3UTIQytR6lbO5a6Oacu5H1/edit?usp=sharing&ouid=110349557058058531133&rtpof=true&sd=true",
       contentBibliografia: "Bibliografia",
@@ -30,7 +30,7 @@ function Curso() {
     {
       name: "Aula 3 - Funções",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/16Dvb20uPD7NPS-guNirL_5HQXtc4yzN3/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/1xGRUDp-jf_ZxTacfkcgpx9fAYOOamZ2J/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "https://docs.google.com/document/d/1tT3kYyTUfy9eXZNaSNSBQFWhl9zpkBOp/edit?usp=sharing&ouid=110349557058058531133&rtpof=true&sd=true",
       contentBibliografia: "Bibliografia",
@@ -40,7 +40,7 @@ function Curso() {
     {
       name: "Aula 4 - Operações Matemáticas",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/1K4bVwL-8RNyS3lIiZKYnYFEmJb7nC8SM/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/157ocXSuvw9b3vV1VLbXpcN19_cPnILoq/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "https://docs.google.com/document/d/1P2jYWEk43Q-1NjCQsvEJKhxfC0uhJBwJ/edit?usp=sharing&ouid=110349557058058531133&rtpof=true&sd=true",
       contentBibliografia: "Bibliografia",
@@ -50,7 +50,7 @@ function Curso() {
     {
       name: "Aula 5 - Operadores Lógicos",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/1ebAaPgldxtER581dHJLtvzf6X80CIARw/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/12DxLlbJ7fYMt0zYHWW7Tn6oO-eVwGUPZ/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "https://docs.google.com/document/d/1k-BkD41ZIu1DRmbhqOsAWa1rvq6wGLO_/edit?usp=sharing&ouid=110349557058058531133&rtpof=true&sd=true",
       contentBibliografia: "Bibliografia",
@@ -60,7 +60,7 @@ function Curso() {
     {
       name: "Aula 6 - Funções Matemáticas",
       contentVideo: "Vídeo",
-      linkVideo: "https://drive.google.com/file/d/1ceabgkl-0eemo2dRdm1Orw_JfJGZIVte/view?usp=sharing",
+      linkVideo: "https://drive.google.com/file/d/1GTu2rJfXooamiIbnN7lc1WSl8XEuE4gk/view?usp=sharing",
       contentDoc: "Documentação",
       linkDoc: "https://docs.google.com/document/d/1_WL3-jGQC0PHVHAfC6Qz2hw9bYFXaKxA/edit?usp=sharing&ouid=110349557058058531133&rtpof=true&sd=true",
       contentBibliografia: "Bibliografia",
